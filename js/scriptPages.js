6541466c49a9d9103a5818ec2f58f752e83cadfa
Anas Nady <eng.anas.nady@gmail.com>
2022-09-07 11:19:22 +0200

    Added Channel Section To All Pages

--- js/scriptPages.js
+++ js/scriptPages.js
@@ -41,3 +41,29 @@ tipsQuestion.forEach((e) => {
     e.target.classList.add("active");
   });
 });
+
+// Scroll Top Button
+let scrollBtn = document.querySelector(".scroll-btn");
+
+window.onscroll = function () {
+  if (window.scrollY >= 500) {
+    scrollBtn.style.opacity = 1;
+  } else {
+    scrollBtn.style.opacity = 0;
+  }
+};
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0 });
+});
+
+// navigation bullets
+
+let bullets = document.querySelectorAll(".nav-bullets .bullet");
+
+bullets.forEach((bullet) => {
+  bullet.addEventListener("click", (e) => {
+    let bulletChecked = document.querySelector(e.target.dataset.section);
+    bulletChecked.scrollIntoView();
+    console.log(bulletChecked.offsetHeight);
+  });
+});
